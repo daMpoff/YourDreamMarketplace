882422f436b95caa5aec60513866c75b0e7b9310
daMpoff <11projectone@gmail.com>
2024-02-28 02:40:47 +0300

    Добавлено отображение товаров

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace YourDreamMarketplace
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public short Stars { get; set; }
+        public double Price { get; set; }
+        public string ImgPath { get; set; }
+        public Product(int iD, string name, short stars, double price, string imgPath)
+        {
+            ID = iD;
+            Name = name;
+            Stars = stars;
+            Price = price;
+            ImgPath = imgPath;
+        }
+    }
+}
---
+++ ViewModels/MainTabViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace YourDreamMarketplace
+{
+    public class MainTabViewModel : INotifyPropertyChanged
+    {
+        public List<Product> Products { get; set; }
+
+        public MainTabViewModel()
+        {
+            Products = new List<Product>();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            Products.Add(new Product(10, "Чайник", 5, 1200, @"/Resources/Images/kettle.png"));
+            Products.Add(new Product(10, "Чайник", 5, 1200, @"/Resources/Images/kettle.png"));
+            Products.Add(new Product(10, "Колонка", 5, 1200, @"/Resources/Images/speaker.png"));
+            Products.Add(new Product(10, "Чайник", 5, 1200, @"/Resources/Images/kettle.png"));
+            Products.Add(new Product(10, "Чайник", 5, 1200, @"/Resources/Images/kettle.png"));
+            Products.Add(new Product(10, "Чайник", 5, 1200, @"/Resources/Images/kettle.png"));
+            Products.Add(new Product(10, "Чайник", 5, 1200, @"/Resources/Images/kettle.png"));
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
