0f81152e4d0281ebbb033ed577c9947981f46645
Daniel <11projectone@gmail.com>
2024-02-29 15:11:22 +0300

    Добавление отзывов

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,6 +7,15 @@
         public short Stars { get; set; }
         public double Price { get; set; }
         public string ImgPath { get; set; }
+        public int NumberOfReviews
+        {
+            get
+            {
+                return Reviews.Count;
+            }
+            set { }
+        }
+        public List<Review> Reviews { get; set; }
         public Product(int id, string name, short stars, double price, string imgPath)
         {
             ID = id;
---
+++ Models/Review.cs
@@ -0,0 +1,81 @@
+﻿namespace YourDreamMarketplace
+{
+    public class Review
+    {
+        private byte stars;
+        private int idReview, idUser;
+        private DateTime dateOfReview;
+        public int IdReview
+        {
+            get
+            {
+                return idReview;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    idReview = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Ошибка, Id меньше 0!");
+                }
+            }
+        }
+        public int IdUser
+        {
+            get
+            {
+                return idUser;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    idUser = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Ошибка, ID пользователя не может быть меньше 0!");
+                }
+            }
+        }
+        public DateTime DateOfReview
+        {
+            get
+            {
+                return dateOfReview;
+            }
+            set
+            {
+                if (dateOfReview > DateTime.MinValue && dateOfReview < DateTime.MaxValue)
+                {
+                    dateOfReview = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Ошибка, дата отзыва указана неверно!");
+                }
+            }
+        }
+        public byte Stars
+        {
+            get
+            {
+                return stars;
+            }
+            set
+            {
+                if (value > 0 && value <= 5)
+                {
+                    stars = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Ошибка, Рейтинг не может быть меньше 0 или меньше 5!");
+                }
+            }
+        }
+    }
+}
